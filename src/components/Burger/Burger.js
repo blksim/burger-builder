22d0d4eb838802966ea0178d4f19aa80d111583d
blksim <blakesim3@gmail.com>
2020-07-16 18:27:29 +0900

    Calculate the ingredient sum dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,22 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => { 
-    const transformedIngredients = Object.keys(props.ingredients) // 키(재료명)만 추출한 배열 
+    let transformedIngredients = Object.keys(props.ingredients) // 키(재료명)만 추출한 배열 
         .map(igKey => { // 각 배열 요소(재료명)마다 
             // props.ingredients[igKey] == 재료 수량 만큼 
             // 키, 타입 속성을 포함한 JSX element 배열을 생성하여 반환 
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
-            });
-        });
+            }); // 어떻게 디버깅하나?
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []); // concat nested array element into transformedIngredients -> accummulate into empty new array 
+        console.log(transformedIngredients);
+
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients!</p>;
+        }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
