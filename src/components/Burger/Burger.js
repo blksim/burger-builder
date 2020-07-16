42f4ebf7f70839cb10d052d39da2c55bf3a84b7b
blksim <blakesim3@gmail.com>
2020-07-16 18:08:00 +0900

    Output burger ingridents dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) => {
+const burger = (props) => { 
+    const transformedIngredients = Object.keys(props.ingredients) // 키(재료명)만 추출한 배열 
+        .map(igKey => { // 각 배열 요소(재료명)마다 
+            // props.ingredients[igKey] == 재료 수량 만큼 
+            // 키, 타입 속성을 포함한 JSX element 배열을 생성하여 반환 
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
