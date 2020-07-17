0e74b8f23b155256efbd825c1cda11a8cc1a9c93
blksim <blakesim3@gmail.com>
2020-07-17 15:48:48 +0900

    fixup! fixup! fixup! Create order summary modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
-import BurgerBuilder from './container/BurgerBuilder/BugerBuilder';
+import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
