b4baf7a56602a2feb3fd9b5b1ecd3d73986caca1
blksim <blakesim3@gmail.com>
2020-07-17 18:14:23 +0900

    Add a toggle button to the sidedrawer component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,17 +7,25 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer : true
+        showSideDrawer : false
     }
 
     sideDrawerClosedHandler = () => {
         this.setState({ showSideDrawer: false });
     }
 
+    sideDrawerToggleHandler = () => {
+        // you shouldn't do like below because async nature of setState, this may lead to unexpected outcomes.
+        //this.setState({ showSideDrawer: !this.state.showSideDrawer });
+        this.setState((prevState) => { 
+            return { showSideDrawer: !prevState.showSideDrawer };
+        }); // this is the clean way of setting state when it depends on the old state.    
+    }
+
     render () {
         return (    
         <Auxiliary>
-            <Toolbar />
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
             <SideDrawer 
                 open={this.state.showSideDrawer} 
                 closed={this.sideDrawerClosedHandler}/>
