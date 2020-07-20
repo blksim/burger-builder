b8aded6d813359ba3f4e4c48043a5b1b3f5cd367
blksim <blakesim3@gmail.com>
2020-07-20 14:46:59 +0900

    Remove old interceptors

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
+
   render() {
     return(
       <div>
         <Layout>
-          <BurgerBuilder></BurgerBuilder>
+          <BurgerBuilder/>
         </Layout>
       </div>
     );
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,14 +9,30 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
         state = {
             error: null
         }
+        // we'll call componentWillMount again and again of course because the class component we return it is 
+        // higher order component is created every time this is wrapped around an existing component,
+        // so every time we call withErrorHnadler on the element we're exporting as we do in the burger builder.
+        // so we're actually attatching multiple interceptors in our application and we're attatching them to the same axios instance.
+        // once we have more pages where we might use withErrorHandler, we of course create this instance here multiple times,
+        // this component here and therefore all the old interceptors, so all the interceptors we set up when we wrapped this around another component which might not be needed anymore still exsist.
+        // so we have a lot of that interceptor sitting in  memory which actually are not that but which still react to our requests and in the wors case, they lead to errors or do somehow change the state of our app 
+        // but even in the best case, they leak memory because that's code that still runs that is not required anymore.
+        // so we should actually remove the interceptors when this component gets unmounted.
         componentWillMount () {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({ error: null });
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({ error: error });
             });
+        }
+
+        // Now as the name suggests, this is a lifecycle method which is executed at the point of time a component isn't required anymore.
+        // we got this setup in componentWillUnmount and we should remove our interceptors with that preventing memory leaks.
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
